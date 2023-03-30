a0a59d9510989eb8939f32d418d7f87780cb340b
henrihyv <henri.hyvarinen1@gmail.com>
2023-03-30 09:46:08 +0300

    lisätty pin-koodin salaus

--- backend/models/kortti_model.js
+++ backend/models/kortti_model.js
@@ -1,5 +1,9 @@
 const db = require('../database');
 
+const bcrypt = require('bcryptjs');
+
+const saltRounds=10;
+
 const kortti = {
   getAll: function(callback) {
     return db.query('select * from Kortti', callback);
@@ -8,21 +12,22 @@ const kortti = {
     return db.query('select * from kortti where idKortti=?', [id], callback);
   },
   add: function(Kortti, callback) {
-    return db.query(
-      'insert into Kortti (idKortti,PIN,idAsiakas,idTili,Debit,Credit) values(?,?,?,?,?,?)',
-      [Kortti.idKortti, Kortti.PIN, Kortti.idAsiakas, Kortti.idTili,Kortti.Debit, Kortti.Credit],
-      callback
-    );
+    
+      bcrypt.hash(Kortti.PIN, saltRounds, function(err, hashed_PIN) {
+        return db.query('insert into Kortti (idKortti,PIN,idAsiakas,idTili,Debit,Credit) values(?,?,?,?,?,?)',
+      [Kortti.idKortti, hashed_PIN, Kortti.idAsiakas, Kortti.idTili,Kortti.Debit, Kortti.Credit],
+      callback);
+      });
   },
   delete: function(id, callback) {
     return db.query('delete from Kortti where idKortti=?', [id], callback);
   },
   update: function(id, Kortti, callback) {
-    return db.query(
-      'update Kortti set PIN=?,Debit=?, Credit=? where idKortti=?',
-      [Kortti.PIN, Kortti.Debit, Kortti.Credit, id],
-      callback
-    );
-  }
+    bcrypt.hash(Kortti.PIN, saltRounds, function(err, hashed_PIN) {
+    return db.query('update Kortti set PIN=?,Debit=?, Credit=? where idKortti=?',
+      [hashed_PIN, Kortti.Debit, Kortti.Credit, id],
+      callback);
+  });
+},
 };
 module.exports = kortti;
