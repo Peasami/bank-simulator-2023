fbfa877a73136212ce4aee011ae6790bb3029a7f
Jylant <t2jyan00@students.oamk.fi>
2023-03-30 12:56:35 +0300

    perusjuttuja

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-<<<<<<< HEAD
 const usersRouter = require('./routes/users');
-<<<<<<< HEAD
 var tilitapahtumatRouter = require('./routes/tilitapahtumat');
-=======
-=======
 var tiliRouter = require('./routes/tili');
->>>>>>> c1f018e24f91c3d99e7f8d872291354c63a5c25f
 var asiakasRouter = require('./routes/asiakas');
->>>>>>> main
 
 const tili_asiakasRouter = require('./routes/tili_asiakas');
 
@@ -26,16 +20,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-<<<<<<< HEAD
 app.use('/users', usersRouter);
-<<<<<<< HEAD
 app.use('/tilitapahtumat', tilitapahtumatRouter);
-=======
-=======
 app.use('/tili', tiliRouter);
->>>>>>> c1f018e24f91c3d99e7f8d872291354c63a5c25f
 app.use('/asiakas', asiakasRouter);
->>>>>>> main
 app.use('/tili_asiakas', tili_asiakasRouter);
 
 module.exports = app;
