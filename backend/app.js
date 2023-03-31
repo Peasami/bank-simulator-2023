0db9e22155bf109aa423a4e09d3ffa82e0779017
works-IKAMASA <122465195+works-IKAMASA@users.noreply.github.com>
2023-03-31 12:35:21 +0300

    web-token luotu ja testattu

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const dotenv=require('dotenv');
-
+const jwt = require('jsonwebtoken');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
 var tilitapahtumatRouter = require('./routes/tilitapahtumat');
 var tiliRouter = require('./routes/tili');
 var asiakasRouter = require('./routes/asiakas');
@@ -22,10 +22,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use(authenticateToken);
 app.use('/tilitapahtumat', tilitapahtumatRouter);
 app.use('/tili', tiliRouter);
 app.use('/asiakas', asiakasRouter);
 app.use('/tili_asiakas', tili_asiakasRouter);
 app.use('/kortti', korttiRouter)
-module.exports = app;
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+  
+    console.log("token = "+token);
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+      console.log(err)
+  
+      if (err) return res.sendStatus(403)
+  
+      req.user = user
+  
+      next()
+    })
+  }
+
+  module.exports = app;
---
+++ backend/routes/login.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const login = require('../models/login_model');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+router.post('/', 
+  function(request, response) {
+    if(request.body.idKortti && request.body.PINkoodi){
+      const kortti = request.body.idKortti;
+      const pinni = request.body.PINkoodi;
+      
+        login.checkPIN(kortti, function(dbError, dbResult) {
+          if(dbError){
+            response.json(dbError);
+          }
+          else{
+            if (dbResult.length > 0) {
+              bcrypt.compare(pinni,dbResult[0].pin, function(err,compareResult) {
+                if(compareResult) {
+                  console.log("onnistui!");
+                  const token = generateAccessToken({ idKortti: kortti });
+                  response.send(token);
+                }
+                else {
+                    console.log("väärä PINkoodi");
+                    //console.log(dbResult[0].pin);
+                    response.send(false);
+                }			
+              }
+              );
+            }
+            else{
+              console.log("korttia ei löydy");
+              response.send(false);
+            }
+          }
+          }
+        );
+      }
+    else{
+      console.log("idKortti tai PINkoodi puuttuu");
+      response.send(false);
+    }
+  }
+);
+
+function generateAccessToken(idKortti) {
+  dotenv.config();
+  return jwt.sign(idKortti, process.env.MY_TOKEN, { expiresIn: '1800s' });
+}
+
+module.exports=router;
