6bd26ebd93818ceb756618d29abe6aa9dfc26988
santeri <santeri.hyry@gmail.com>
2023-04-14 14:06:34 +0300

    main.cpp aukaisee insertcardwindowin eikä mainwindowia.

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,12 +1,12 @@
 #include "insertcardwindow.h"
-#include "mainwindow.h"
+#include "mainwindow.h" // Debuggausta varten,jos mainwindow pitää saada auki ilman kortinlukijaa
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    InsertCardWindow w;
     w.show();
     return a.exec();
 }
